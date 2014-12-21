169d6b12cc4a4c8037ae169af592fdaaff36e2a1
Lev Shagalov <lewik@interprogma.ru>
2014-12-21 21:58:00 +0300

    bower jquery и бандл для Ui

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,13 +1,17 @@
 <?php
 
-use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Kernel;
 
+/** Class AppKernel */
 class AppKernel extends Kernel
 {
+    /**
+     * @return array
+     */
     public function registerBundles()
     {
-        $bundles = array(
+        $bundles = [
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
@@ -18,9 +22,10 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Lewik\BoBundle\LewikBoBundle(),
             new Lewik\BoManagerBundle\LewikBoManagerBundle(),
-        );
+            new Lewik\UiBundle\LewikUiBundle(),
+        ];
 
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+        if (in_array($this->getEnvironment(), ['dev', 'test'])) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
@@ -30,8 +35,11 @@ class AppKernel extends Kernel
         return $bundles;
     }
 
+    /**
+     * @param LoaderInterface $loader
+     */
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+        $loader->load(__DIR__ . '/config/config_' . $this->getEnvironment() . '.yml');
     }
 }
---
+++ src/Lewik/UiBundle/LewikUiBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Lewik\UiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class LewikUiBundle extends Bundle
+{
+}
